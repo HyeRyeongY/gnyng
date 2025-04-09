4ade33082b44c6335b09f1be5086fe7a7225b5a5
HyeRyeongYoon <dbs123457@gmail.com>
2025-04-09 23:25:12 +0900

    fonts import 방식 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,25 @@
 export default defineNuxtConfig({
   ssr: true,
+
   app: {
     head: {
       title: "YHR",
       meta: [{ name: "description", content: "This is yoonhyeryeong's project." }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&display=swap",
+        },
+      ],
     },
   },
+
   css: ["@/assets/css/reset.scss", "@/assets/css/global.scss"],
+
   runtimeConfig: {
     notionToken: process.env.NOTION_TOKEN,
     notionDatabaseId: process.env.NOTION_DATABASE_ID,
   },
+
+  compatibilityDate: "2025-04-09",
 });
