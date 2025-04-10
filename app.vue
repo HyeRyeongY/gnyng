387738c4729b4e1b410a7dab9ad7380ed51577bd
HyeRyeongYoon <dbs123457@gmail.com>
2025-04-10 23:58:26 +0900

    커서 마우스 있을 때만 출력하는 로직 추가 및 파비콘 변경

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
 <template>
   <NuxtLayout>
-    <Cursor />
+    <Cursor v-if="isMouse" />
     <NuxtPage />
   </NuxtLayout>
 </template>
 <script setup>
 import Cursor from "@/components/Cursor.vue";
+
+import { onMounted, ref } from "vue";
+const isMouse = ref(false);
+
+onMounted(() => {
+  isMouse.value = window.matchMedia("(hover: hover)").matches;
+});
 </script>
