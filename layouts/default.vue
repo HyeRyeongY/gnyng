288b6a25c012461d1750b51e35649daba8a97b68
HyeRyeongYoon <dbs123457@gmail.com>
2025-04-13 00:02:06 +0900

    메뉴 로직 수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,17 @@
     <header>
       <ul class="gnb">
         <li>
-          <a href="#yes"
+          <a href="#yes" @click.prevent="navigateToAnchor('yes')"
             ><em>Y</em><span><strong>Yes.</strong><br />[ 네 ]</span></a
           >
         </li>
         <li>
-          <a href="#here"
+          <a href="#here" @click.prevent="navigateToAnchor('here')"
             ><em>H</em><span><strong>Here.</strong><br />[ 여기 있습니다 ]</span></a
           >
         </li>
         <li>
-          <a href="#ready"
+          <a href="#ready" @click.prevent="navigateToAnchor('ready')"
             ><em>R</em><span><strong>Ready.</strong><br />[ 준비됐습니다 ]</span></a
           >
         </li>
@@ -30,3 +30,23 @@
     </footer>
   </div>
 </template>
+<script setup lang="ts">
+import { useRouter, useRoute } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+function navigateToAnchor(anchorId: string) {
+  const targetPath = "/";
+  const hash = `#${anchorId}`;
+
+  if (route.path !== targetPath) {
+    router.push({ path: targetPath, hash });
+  } else {
+    const el = document.getElementById(anchorId);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+}
+</script>
